993de8b9fbe7471fa932a92b38d8bc90991a8380
Stefan Łodyga <stelod00@gmail.com>
2025-07-22 16:56:10 +0200

    Add player movement

--- player.gd
+++ player.gd
@@ -1,7 +1,8 @@
-extends Node2D
+extends CharacterBody2D
 class_name Player
 
 @export var held_object: HoldableInstance = null
+var speed = 300
 
 func pick_up(object: HoldableInstance):
 	if held_object != null:
@@ -15,3 +16,8 @@ func drop():
 	Stage.add_child(self.held_object)
 	self.held_object.global_position = global_position
 	self.held_object = null
+
+func _physics_process(_delta: float) -> void:
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_dir * speed
+	move_and_slide()
